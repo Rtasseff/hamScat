d41df58f2862c6bcf75c5bdf2cd18d65a6422cc2
rtasseff <rtasseff@gmail.com>
2013-09-03 17:04:40 +0200

    enforced passing of sampleMeta so we always know the sample size

--- creatJobList_FM.py
+++ creatJobList_FM.py
@@ -6,7 +6,7 @@ FM = 'HMST'
 label = 'DF4_2_miRNA_HMST_20130903'
 myPath = '/titan/cancerregulome9/ITMI_PTB/users/rtasseff/runMP'
 dataPath = '/titan/cancerregulome9/ITMI_PTB/users/rtasseff/DF4/DF4_2/miRNA'
-tagPath = 'sampleMeta.dat' # can be commented out if FM != HMST
+tagPath = 'sampleMeta.dat' 
 
 
 index = np.loadtxt(dataPath+'/index.tab',dtype=str)#[:10]
@@ -20,6 +20,6 @@ for i in range(len(index)):
 			dataPath+'/'+index[i,5]+' '+ \
 			index[i,4]+' '+ \
 			dataPath+'/'+tagPath+'\n')
-	#		'\n')
+
 
 f.close()
