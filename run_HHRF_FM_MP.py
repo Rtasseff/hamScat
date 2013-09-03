d5a0b68f80721ff404aef52655ed9f48db2f507d
rtasseff <rtasseff@gmail.com>
2013-09-03 12:36:41 +0200

    do not calc n=1

--- run_HHRF_FM_MP.py
+++ run_HHRF_FM_MP.py
@@ -16,18 +16,27 @@ matPath = sys.argv[2]
 # number of variants in transcript data matrix
 nVar = int(sys.argv[3])
 
-# get X from bianary matrix
-X = hamScat.readBiMat(matPath,nVar)
-
-
-hhrScore = hamScat.calcHHRef(X)
-
-
-# print to std out for golem to catch 
 out = 'B:HHRF:'+transID
-for i in range(len(hhrScore)):
-	out = '%s\t%05.4E' % (out,hhrScore[i])
-out = out+'\n'
+
+if nVar>1:
+
+	# get X from bianary matrix
+	X = hamScat.readBiMat(matPath,nVar)
+
+
+	hhrScore = hamScat.calcHHRef(X)
+
+
+	# print to std out for golem to catch 
+	for i in range(len(hhrScore)):
+		out = '%s\t%05.4E' % (out,hhrScore[i])
+	out = out+'\n'
+else:
+	X = hamScat.readBiMat(matPath,nVar)
+	for i in range(len(X)):
+		out = 'out\tnan'
+	out = out+'\n'		
+
 
 print out
 
