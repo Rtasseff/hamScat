c2b94c3798088dd068a25570e04b7f209b73621b
rtasseff <rtasseff@gmail.com>
2013-09-03 12:35:50 +0200

    not sure

--- creatJobList_FM.py
+++ creatJobList_FM.py
@@ -3,9 +3,9 @@ import numpy as np
 #FM = 'MINA'
 #FM = 'HHRF'
 FM = 'HMST'
-label = 'DF4_2_HMST_20130708'
+label = 'DF4_2_miRNA_HMST_20130903'
 myPath = '/titan/cancerregulome9/ITMI_PTB/users/rtasseff/runMP'
-dataPath = '/titan/cancerregulome9/ITMI_PTB/users/rtasseff/DF4/DF4_2/PPC'
+dataPath = '/titan/cancerregulome9/ITMI_PTB/users/rtasseff/DF4/DF4_2/miRNA'
 tagPath = 'sampleMeta.dat' # can be commented out if FM != HMST
 
 
@@ -14,11 +14,12 @@ index = np.loadtxt(dataPath+'/index.tab',dtype=str)#[:10]
 f=open('golemJobList_'+label+'.txt','w')
 
 for i in range(len(index)):
-	f.write('1 /tools/bin/python2.7 '+myPath+'/run_'+FM+'_FM_MP.py '+ \
-		index[i,0]+' '+ \
-		dataPath+'/'+index[i,5]+' '+ \
-		index[i,4]+' '+ \
-		dataPath+'/'+tagPath+'\n')
-#		'\n')
+	if int(index[i,4])>1:
+		f.write('1 /tools/bin/python2.7 '+myPath+'/run_'+FM+'_FM_MP.py '+ \
+			index[i,0]+' '+ \
+			dataPath+'/'+index[i,5]+' '+ \
+			index[i,4]+' '+ \
+			dataPath+'/'+tagPath+'\n')
+	#		'\n')
 
 f.close()
