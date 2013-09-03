0d8cf0c697d55653c9fb2d960e3cc2a5bbc43111
rtasseff <rtasseff@gmail.com>
2013-09-03 15:56:30 +0200

    new way to deal with missing, without exact indices

--- creatJobList_scat_missing.py
+++ creatJobList_scat_missing.py
@@ -1,23 +1,23 @@
 import numpy as np
 
-label = 'DF4_2_hamScat_F_20130712_missed'
+label = 'DF4_2_hamScat_addFeat_2_missed_20130903_test'
 myPath = '/titan/cancerregulome9/ITMI_PTB/users/rtasseff/runMP'
 dataPath = '/titan/cancerregulome9/ITMI_PTB/users/rtasseff/DF4/DF4_2/PPC'
-member = 'F'
-tagPath = 'sampleMeta.dat'
-index = np.loadtxt(dataPath+'/index.tab',dtype=str)#[:10]
-missed = np.loadtxt(dataPath+'/results/DF4_2_20130712_scat_report_F_missing.dat',dtype=int)
-index = index[missed]
+tagPath = dataPath+'/sampleMeta.dat'
+index = np.loadtxt(dataPath+'/index.tab',dtype=str)
+selFeatPath = myPath+'/sampleMeta_selectFeature_test.dat'
+missing = np.loadtxt(dataPath+'/results/DF4_2_additional2_20130830_hamScat.tsv_missing.dat',dtype=str)
 
 f=open('golemJobList_'+label+'.txt','w')
 
 for i in range(len(index)):
-	f.write('1 /tools/bin/python2.7 '+myPath+'/run_hamScat_MP.py '+ \
-		index[i,0]+' '+ \
-		dataPath+'/'+index[i,5]+' '+ \
-		index[i,4]+' '+ \
-		dataPath+'/'+tagPath+' '+ \
-		member+'\n')
+	if np.any(index[i,0]==missing):
+		f.write('1 /tools/bin/python2.7 '+myPath+'/run_hamScat_MP.py '+ \
+			index[i,0]+' '+ \
+			dataPath+'/'+index[i,5]+' '+ \
+			index[i,4]+' '+ \
+			tagPath+' '+ \
+			selFeatPath+'\n')
 
 f.close()
 
