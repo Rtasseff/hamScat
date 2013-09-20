65898d80355e0e1d145c13d2f29840b0fc36a04b
rtasseff <rtasseff@gmail.com>
2013-09-20 11:52:32 +0200

    add 2 termporary files to do a custome run, its pretty funky and sloppy and should be removed after the run is done

---
+++ creatJobList_scat_cust.py
@@ -0,0 +1,23 @@
+import numpy as np
+
+label = 'DF4_2_PPC_hamScat_20130919'
+myPath = '/titan/cancerregulome9/ITMI_PTB/users/rtasseff/runMP'
+dataPath = '/titan/cancerregulome9/ITMI_PTB/users/rtasseff/DF4/DF4_2/PPC'
+tagPath = dataPath+'/sampleMeta.dat'
+#tmpInd = [2,4,7]
+index = np.loadtxt(dataPath+'/index.tab',dtype=str)#[tmpInd]
+selFeatPath = dataPath+'/sampleMeta_selectFeature.dat'
+
+f=open('golemJobList_'+label+'.txt','w')
+
+for i in range(len(index)):
+	f.write('1 /tools/bin/python2.7 '+myPath+'/run_hamScat_MP_cust.py '+ \
+		index[i,0]+' '+ \
+		dataPath+'/'+index[i,5]+' '+ \
+		index[i,4]+' '+ \
+		tagPath+' '+ \
+		selFeatPath+'\n')
+
+f.close()
+
+
